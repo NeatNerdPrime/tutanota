e63c0a1dae903cd196a2a719b4882d73616ea9cb
mpfau <mpfau@tutao.de>
2020-05-25 17:07:21 +0200

    disable vertical scrolling for multiple columns, fixes #2073

--- src/gui/main-styles.js
+++ src/gui/main-styles.js
@@ -248,7 +248,7 @@ styles.registerStyle('main', () => {
 		'.height-100p': {height: "100%"},
 
 
-		'.view-columns': {'overflow-x': 'hidden'},
+		'.view-columns': {'overflow': 'hidden'},
 
 		'.view-column': {'will-change': 'transform'},
 
